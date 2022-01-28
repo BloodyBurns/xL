6cdc45549c05de2b41c050b7ab10db978c25f7c0
BloodyBurns <83672765+BloodyBurns@users.noreply.github.com>
2022-01-27 20:39:21 -0500

    Update PrisonLifeX.lua

--- PrisonLifeX.lua
+++ PrisonLifeX.lua
@@ -974,7 +974,8 @@ getgenv().NotifyX = function(message, lenght)
 		local Timer = Instance.new("Frame")
 		local close = Instance.new("TextButton")
 		local Tween = game:GetService("TweenService"):Create(Timer, TweenInfo.new(lenght), {Position = UDim2.new(1, 0, 0.991, 0)})
-
+		if #message >= 20 then
+				tb2.TextScaled = true
 		Noti.Name = "Noti"
 		Noti.Parent = Notifications
 		Noti.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
